4e956885b1256b469312a0791c04b74a62f5a902
Liang Hong <lianghongbuaa@gmail.com>
2016-12-15 18:34:59 +0800

    Add.

--- src/test/java/com/thoughtworks/gaia/exam/ExamServiceFunctionalTest.java
+++ src/test/java/com/thoughtworks/gaia/exam/ExamServiceFunctionalTest.java
@@ -4,7 +4,6 @@ import com.thoughtworks.gaia.GaiaApplication;
 import com.thoughtworks.gaia.common.constant.EnvProfile;
 import com.thoughtworks.gaia.exam.entity.Exam;
 import com.thoughtworks.gaia.exam.service.ExamService;
-import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 /**
  * Created by hliang on 15/12/2016.
@@ -33,8 +34,8 @@ public class ExamServiceFunctionalTest {
     @Test
     public void should_get_product_with_id() throws Exception {
         exam.setName("test");
-        exam.setStartTime(new DateTime(2016,12,16,0,0));
-        exam.setEndTime(new DateTime(2016,12,20,0,0));
+        exam.setStartTime(new Date());
+        exam.setEndTime(new Date());
         exam.setLogicNum(12);
         exam.setProgramNum(10);
         exam.setState(0);
