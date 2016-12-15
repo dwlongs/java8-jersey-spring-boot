b858a1a843a252d3a60fe362cf5a134d87ba0b41
Weilong Du <wldu@CNwldu.local>
2016-12-15 18:01:42 +0800

    modify create_exam_table file name

---
+++ src/main/java/com/thoughtworks/jwt/JsonAccessToken.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.jwt;
+
+/**
+ * Created by wldu on 12/15/16.
+ */
+public class JsonAccessToken {
+}
---
+++ src/test/java/test/functional/jwt/JsonAccessTokenFunctionalTest.java
@@ -0,0 +1,7 @@
+package test.functional.jwt;
+
+/**
+ * Created by wldu on 12/15/16.
+ */
+public class JsonAccessToken {
+}
